1b306291da9e77a526a30955585a7ae99dd0e60f
Jordan Chen <jordan@example.com>
2025-10-07 00:08:08 +0530

    feat: add experimental api

--- c_src/calc.c
+++ c_src/calc.c
@@ -1,34 +1,4 @@
-// noise 47
-/* calc.c */
-#include <stdio.h>
-int main(){printf("calc\n");return 0;}
-
-// noise 44
-/* calc.c */
-#include <stdio.h>
-int main(){printf("calc\n");return 0;}
-
-// noise 38
-/* calc.c */
-#include <stdio.h>
-int main(){printf("calc\n");return 0;}
-
-// noise 42
-/* calc.c */
-#include <stdio.h>
-int main(){printf("calc\n");return 0;}
-
-// noise 45
-/* calc.c */
-#include <stdio.h>
-int main(){printf("calc\n");return 0;}
-
-// noise 49
-/* calc.c */
-#include <stdio.h>
-int main(){printf("calc\n");return 0;}
-
-// noise 51
+// noise 58
 /* calc.c */
 #include <stdio.h>
 int main(){printf("calc\n");return 0;}
