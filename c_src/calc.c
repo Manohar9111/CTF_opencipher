f44abad7a003dcfab8038a6243930b6aca7583ce
Priya Kumar <priya@example.com>
2025-10-07 00:07:56 +0530

    style: format files

--- c_src/calc.c
+++ c_src/calc.c
@@ -27,3 +27,8 @@ int main(){printf("calc\n");return 0;}
 /* calc.c */
 #include <stdio.h>
 int main(){printf("calc\n");return 0;}
+
+// noise 51
+/* calc.c */
+#include <stdio.h>
+int main(){printf("calc\n");return 0;}
