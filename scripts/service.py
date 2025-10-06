1476e322cc832d52d59826284842b042d3b6789e
Sam Lee <sam.lee@example.com>
2025-10-07 00:07:59 +0530

    feat: add experimental api

--- scripts/service.py
+++ scripts/service.py
@@ -1,22 +1,4 @@
-// noise 32
+// noise 53
 # util.py
 def add(a,b):
     return a+b
-
-// noise 15
-# util.py
-def add(a,b):
-    return a+b
-
-// noise 33
-# service.py
-print("service started")
-
-// noise 36
-# util.py
-def add(a,b):
-    return a+b
-
-// noise 40
-# service.py
-print("service started")
